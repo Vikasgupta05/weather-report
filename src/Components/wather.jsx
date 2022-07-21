2de87a0dfcb2d3db8976f027adc85d9670bc29ba
vikas <vikasnwrdr2001@gmail.com>
2022-07-21 23:32:11 +0530

    set pressure and humdity

--- src/Components/wather.jsx
+++ src/Components/wather.jsx
@@ -15,8 +15,10 @@ import sunny from '../Images/sunny.png'
 export const Weather = () => {
 
     const [search , setSearch] = useState()
-
     const [weather , setWeather] = useState()
+    const [pressure ,  setPressure] = useState()
+    const [humdity ,  setHumdity] = useState()
+
 
     useEffect(() => {
         data()
@@ -29,13 +31,19 @@ export const Weather = () => {
             console.log( "log",res.data.coord.lon)
             console.log( "lat",res.data.coord.lat)
             setWeather(res.data.main.temp)
+            setPressure(res.data.main.pressure)
+            setHumdity(res.data.main.humidity)
+
+
+
+            
         })
     }
 
     const get_lat_Log = (lat , lon) => {
         axios.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=38d793fd557896e87ffc86c502e4dac0&units=metric`).then(function(res) {
 
-        })
+        })  
     }
 
 
@@ -64,6 +72,31 @@ export const Weather = () => {
             </button>
 
 
+
+            <div>
+                {/* {
+                    weather?.map((e, i) => {
+                        // console.log(e.dt)
+                        // const dateTimeStr = new Date(e.dt*1000).toLocaleString("en-US",{weekday:"long"}).slice(0,3);
+                        return (
+                        <div key={i} className="Weather_8days">
+                            <div className='Weatherdetails'>
+                            <p className="weekdays">{dateTimeStr}</p>
+                            <span className="span maxtemp">{e.temp.max.toFixed()}&deg;</span>
+                            <span className="span mintemp">{e.temp.min.toFixed()}&deg;</span>
+                            </div>
+                            <div className="image_div">
+                            <img className="image" src={(e.weather[0].main == "Clear") ? sunny : (e.weather[0].main == "Rain") ? rainy : cloudy} />
+                            <p className='Weather_status'>{e.weather[0].main}</p>
+                            </div>
+                        </div>
+                        )
+                    })
+                } */}
+
+            </div>
+
+
             <div className="dispaly_weather">
                 <div className="temp_div">
 
@@ -79,12 +112,31 @@ export const Weather = () => {
                          <img src={sunny} alt="" />
 
                     </span>
+
                 </div>
 
                 <div className="Graph1">
 
                     <Graph1/>
                 </div>
+
+                <hr />
+
+                <div className="pres_humi">
+                    <span>
+                        <p>
+                            Pressure :  <p1>  {pressure} </p1>
+                        </p>
+                    </span>
+
+                    <span>
+                        <p>
+                        Humidity :   <p1>  {humdity}% </p1>
+                        </p>
+                    </span>
+                </div>
+
+        
 
             </div>
         </div>
